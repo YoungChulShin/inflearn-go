0e9de9b6941794ac4610750acb5cb240c7625257
YoungChulShin <go1323@gmail.com>
2023-01-15 14:55:45 +0900

    패키지

--- src/section3/feature1.go
+++ src/section3/feature1.go
@@ -1,5 +1,6 @@
 package main
 
 func feature1() {
-
+	// gofmt -h: 사용법
+	// gofmt -w: 원본 파일에 반영
 }
---
+++ src/section4/package1.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"section4/lib"
+)
+
+func main() {
+	// var name string
+	// fmt.Println("이름은? : ")
+	// fmt.Scanf("%s", &name)
+
+	// fmt.Fprintf(os.Stdout, "Hi %s\n", name)
+
+	fmt.Println(lib.CheckNum(15))
+}
